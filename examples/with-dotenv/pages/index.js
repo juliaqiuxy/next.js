97078c8cdaf883ba4a19d205da5f52c53593dc71
Julia Qiu <juliaqiuxy@gmail.com>
2019-06-17 22:54:58 -0700

    simplify with-dotenv example

--- examples/with-dotenv/pages/index.js
+++ examples/with-dotenv/pages/index.js
@@ -1,1 +1,4 @@
-export default () => <div>{process.env.TEST}</div>
+import getConfig from 'next/config';
+const { publicRuntimeConfig: { TEST } } = getConfig();
+
+export default () => (<div>{TEST}</div>);
